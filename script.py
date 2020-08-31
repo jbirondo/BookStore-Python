7d7626f4855c8573f6eeb06cd06681800501002e
Eugene Birondo <jbirondo@gmail.com>
2020-08-31 16:28:45 -0700

    Frontend BookStore

--- script.py
+++ script.py
@@ -5,13 +5,13 @@ window= Tk()
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
-l2 = Label(window, text="Title")
+l2 = Label(window, text="Author")
 l2.grid(row=0, column=2)
 
-l3 = Label(window, text="Title")
+l3 = Label(window, text="Year")
 l3.grid(row=1, column=0)
 
-l4 = Label(window, text="Title")
+l4 = Label(window, text="ISBN")
 l4.grid(row=1, column=4)
 
 
