8ecad106b82dfe9f8de115a3ba245125e4bf2afe
Eugene Birondo <jbirondo@gmail.com>
2020-08-31 16:27:41 -0700

    Frontend BookStore

--- script.py
+++ script.py
@@ -2,7 +2,17 @@ from tkinter import *
 
 window= Tk()
 
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
 
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
 
 
 window.mainloop()
