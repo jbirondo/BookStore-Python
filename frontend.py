96f2769244eebc754aa979e5a00a095b66aa6699
Eugene Birondo <jbirondo@gmail.com>
2020-08-31 21:08:05 -0700

    Connecting backend to frontend

--- frontend.py
+++ frontend.py
@@ -7,6 +7,10 @@ def view_command():
     list1.delete(0, END)
     for row in backend.view():
         list1.insert(END, row)
+
+def search_command():
+    list1.delete(0, END)
+    
     
 
 l1 = Label(window, text="Title")
@@ -29,11 +33,11 @@ author_text = StringVar()
 e2 = Entry(window, textvariable=author_text)
 e2.grid(row=0, column=3)
 
-year_text = StringVar()
+year_text = IntVar()
 e3 = Entry(window, textvariable=year_text)
 e3.grid(row=1, column=1)
 
-isbn_text = StringVar()
+isbn_text = IntVar()
 e4 = Entry(window, textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
@@ -49,7 +53,7 @@ scroll_bar1.configure(command=list1.yview)
 b1 = Button(window, text="View All", width=12, command=view_command)
 b1.grid(row=2, column=3) 
 
-b2 = Button(window, text="Search entry", width=12)
+b2 = Button(window, text="Search entry", width=12, command=search_command)
 b2.grid(row=3, column=3) 
 
 b3 = Button(window, text="Add entry", width=12)
