de70fe3705b37cb6b8bb988ad4d6562821bf0cd9
Eugene Birondo <jbirondo@gmail.com>
2020-08-31 17:21:39 -0700

    Backend Bookstore

--- backend.py
+++ backend.py
@@ -14,4 +14,23 @@ def insert(title, author, year, isbn):
     conn.commit()
     conn.close()
 
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows= cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",
+                (title, author, year, isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
 connect()
+# insert("The Sea", "John Tablet", 1918, 1111111)
+# print(view())
