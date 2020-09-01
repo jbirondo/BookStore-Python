8021a7976d9c0528d44829c528752f422956a2ab
Eugene Birondo <jbirondo@gmail.com>
2020-08-31 22:00:30 -0700

    Connecting backend to frontend

--- frontend.py
+++ frontend.py
@@ -22,6 +22,23 @@ def add_command():
     backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
+def get_selected_row(event):
+    global selected_tuple
+    index = list1.curselection()[0]
+    selected_tuple=list1.get(index)
+    e1.delete(0, END)
+    e1.insert(END, selected_tuple[1])
+    e2.delete(0, END)
+    e2.insert(END, selected_tuple[2])
+    e3.delete(0, END)
+    e3.insert(END, selected_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END, selected_tuple[4])
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
@@ -59,6 +76,8 @@ scroll_bar1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=scroll_bar1.set)
 scroll_bar1.configure(command=list1.yview)
 
+list1.bind("<<ListboxSelect>>", get_selected_row)
+
 b1 = Button(window, text="View All", width=12, command=view_command)
 b1.grid(row=2, column=3) 
 
@@ -71,7 +90,7 @@ b3.grid(row=4, column=3)
 b4 = Button(window, text="Update", width=12)
 b4.grid(row=5, column=3) 
 
-b5 = Button(window, text="Delete", width=12)
+b5 = Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6, column=3) 
 
 b6 = Button(window, text="Close", width=12)
